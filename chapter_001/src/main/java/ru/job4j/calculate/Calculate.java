c83eca1d5b062e73c447f9d45b4a104c91a6a4d7
anabrikn <anabrikn@gmail.com>
2018-09-30 00:37:33 +0700

    1.3. JavaDoc. Code convention [4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,17 @@
 package ru.job4j.calculate;
 
+/**
+ * Класс для вывода приветствия в консоль
+ * @author anna
+ * @since 29.09.18
+ * @version 1
+ */
+
 public class Calculate {
+	/**
+	* Main
+	* @param args - args.
+	*/
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Anna (anna241292@mail.ru)
+* @version $1$
+* @since 29.09.18
+*/
+package ru.job4j.calculate;
