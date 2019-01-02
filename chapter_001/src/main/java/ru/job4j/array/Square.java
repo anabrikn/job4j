b861e2c7bc404ab6ea2fb3ed1717e301ce5b74ec
anabrikn <anabrikn@gmail.com>
2019-01-02 18:07:57 +0700

    [#33488]

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -12,8 +12,8 @@ public class Square {
      */
     public int[] calculate(int bound) {
         int[] rst = new int[bound];
-        for (int index = 0, i = 1; index < rst.length; index++, i ++) {
-            rst[index] = (int)Math.pow(i, 2);
+        for (int index = 0, i = 1; index < rst.length; index++, i++) {
+            rst[index] = (int) Math.pow(i, 2);
         }
         return rst;
     }
