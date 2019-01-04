4dcc54e5b451e50b8eb19f380ddc2e242c76a460
anabrikn <anabrikn@gmail.com>
2019-01-05 00:55:18 +0700

    [#225]

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -1,8 +1,27 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+/**
+ * @author anna brik
+ * @version 1
+ * @since 04.01.19
+ */
 public class ArrayDuplicate {
+    /**
+     * @param array массив строк.
+     * @return новый массив, который содержит строки в единственном экземпляре.
+     */
     public String[] remove(String[] array) {
-
-        return array;
+        int length = array.length; //
+        for (int i = 0; i < length; i++) { //
+            for (int j = i + 1; j < length; j++) { //
+                if (array[i].equals(array[j])) { //
+                    array[j] = array[length - 1];
+                    length--;
+                    j--;
+                }
+            }
+        }
+        return Arrays.copyOf(array, length);
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -1,4 +1,21 @@
 package ru.job4j.array;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * @author anna brik
+ * @version 1
+ * @since 04.01.19
+ */
 public class ArrayDuplicateTest {
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+        ArrayDuplicate arrayDuplicate = new ArrayDuplicate();
+        String[] input = new String[] {"a", "a", "a", "a", "b", "c", "d", "a"};
+        String[] result = arrayDuplicate.remove(input);
+        String[] expect = new String[] {"a", "d", "c", "b"};
+        assertThat(result, is(expect));
+    }
 }
