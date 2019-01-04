941b09672f2f880ae6612dd729412372f1380893
anabrikn <anabrikn@gmail.com>
2019-01-04 20:37:39 +0700

    [#53859]

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -12,23 +12,13 @@ public class MatrixCheck {
      * @return результат проверки, что все элементы по диагоналям равны true или false.
      */
     public boolean mono(boolean[][] data) {
-        boolean result;
-        boolean diagonal1 = true;
-        boolean diagonal2 = true;
-
+        boolean result = true;
         for (int i1 = data.length - 1, i2 = 0, j = data.length - 1; i1 > 0; i1--, i2++, j--) {
-            if (data[i1][j] != data[i1 - 1][j - 1]) {
-                diagonal1 = false;
-                break;
-            }
-
-            if (data[i2][j] != data[i2 + 1][j - 1]) {
-                diagonal2 = false;
+            if (data[i1][j] != data[i1 - 1][j - 1] || data[i2][j] != data[i2 + 1][j - 1]) {
+                result = false;
                 break;
             }
         }
-
-        result = diagonal1 && diagonal2;
         return result;
     }
 }
