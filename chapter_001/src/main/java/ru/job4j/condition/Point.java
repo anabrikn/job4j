4268a6bb52bace91828872f2e3e7539a8464f929
Anna <anabrikn@gmail.com>
2019-09-30 22:38:08 +0300

    [#122587]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -12,8 +12,8 @@ public class Point {
         return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
     }
     public static void main(String[] args) {
-        Point a = new Point(0, 1);
-        Point b = new Point(2, 5);
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
         double result = a.distanceTo(b);
         System.out.println("Расстояние между точками А и Б: " + result);
     }
