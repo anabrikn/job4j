46fd04c22cac79558caf9c9a9f0780f22d3fb3b6
Anna <anabrikn@gmail.com>
2019-10-07 23:23:07 +0300

    [#396]

--- chapter_002/src/main/java/ru/job4j/newtracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/newtracker/Tracker.java
@@ -2,12 +2,9 @@ package ru.job4j.newtracker;
 import java.util.Random;
 
 public class Tracker {
-    private Item[] items = new Item[100];
+    private final Item[] items = new Item[100];
     private int position = 0;
-    /**
-     * Метод реализаущий добавление заявки в хранилище
-     * @param item новая заявка
-     */
+
     public Item add(Item item) {
         item.setId(this.generateId());
         this.items[this.position++] = item;
@@ -16,10 +13,12 @@ public class Tracker {
 
     public boolean replace(String id, Item item) {
         boolean isReplase = false;
-        for (int i = 0; i < items.length; i++) {
-            if (this.items[i].getId().equals(id)) {
-                this.items[i] = item; // ???
+        for (int i = 0; i != this.position; i++) {
+            if (items[i].getId().equals(id)) {
+                item.setId(items[i].getId());
+                items[i] = item;
                 isReplase = true;
+                break;
             }
         }
         return isReplase;
@@ -41,11 +40,6 @@ public class Tracker {
         return items[0];
     }
 
-    /**
-     * Метод генерирует уникальный ключ для заявки.
-     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
-     * @return Уникальный ключ.
-     */
     private String generateId() {
         return String.valueOf(System.currentTimeMillis() + new Random(System.currentTimeMillis()).nextLong());
     }
---
+++ chapter_002/src/test/java/ru/job4j/newtrackertest/TrackerTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.newtrackertest;
+
+import ru.job4j.newtracker.*;
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TrackerTest {
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    @Test
+    public void whenReplaceNameThenReturnNewName() {
+        Tracker tracker = new Tracker();
+        Item previous = new Item("test1");
+        tracker.add(previous);
+        Item next = new Item("test2");
+        next.setId(previous.getId());
+        tracker.replace(previous.getId(), next);
+        assertThat(tracker.findById(previous.getId()).getName(), is("test2"));
+    }
+}
