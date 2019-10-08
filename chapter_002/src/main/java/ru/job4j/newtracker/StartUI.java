3b94595f14e5a77bec7876471595e140f2672f98
Anna <anabrikn@gmail.com>
2019-10-09 02:19:41 +0300

    [#784]

--- chapter_002/src/main/java/ru/job4j/newtracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/newtracker/StartUI.java
@@ -72,7 +72,13 @@ public class StartUI {
 
     private void showMenu() {
         System.out.println("Menu.");
-        // добавить остальные пункты меню.
+        System.out.println("0. Add new Item");
+        System.out.println("1. Show all items");
+        System.out.println("2. Edit item");
+        System.out.println("3. Delete item");
+        System.out.println("4. Find item by Id");
+        System.out.println("5. Find items by name");
+        System.out.println("6. Exit Program");
     }
 
 
