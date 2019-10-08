82e881c597f25a9529205591d5a441218844f88a
Anna <anabrikn@gmail.com>
2019-10-08 20:56:07 +0300

    [#396]

--- chapter_002/src/test/java/ru/job4j/newtrackertest/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/newtrackertest/TrackerTest.java
@@ -3,6 +3,7 @@ package ru.job4j.newtrackertest;
 import ru.job4j.newtracker.*;
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
@@ -24,5 +25,96 @@ public class TrackerTest {
         next.setId(previous.getId());
         tracker.replace(previous.getId(), next);
         assertThat(tracker.findById(previous.getId()).getName(), is("test2"));
+    }
+
+
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItemFindAll() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        assertThat(tracker.findAll()[0], is(item));
+    }
+
+    @Test
+    public void whenDeleteItemThenNewItems() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("test1");
+        Item second = new Item("test2");
+        Item third = new Item("test3");
+        Item fourth = new Item("test4");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(third);
+        tracker.add(fourth);
+        Item[] guessArray = {first, third, fourth};
+        tracker.delete(second.getId());
+        assertThat(tracker.findAll(), is(guessArray));
+    }
+
+    @Test
+    public void whenAllItemsAreFound() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("test1");
+        Item second = new Item("test2");
+        Item third = new Item("test3");
+        Item fourth = new Item("test4");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(third);
+        tracker.add(fourth);
+        Item[] guessArray = {first, second, third, fourth};
+
+        assertThat(tracker.findAll(), is(guessArray));
+    }
+
+    @Test
+    public void whenItemsAreFoundByName() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("test1");
+        Item second = new Item("test2");
+        Item third = new Item("test3");
+        Item fourth = new Item("test2");
+        Item fifth = new Item("test5");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(third);
+        tracker.add(fourth);
+        tracker.add(fifth);
+        Item[] resultArray = {second, fourth};
+        assertThat(tracker.findByName("test2"), is(resultArray));
+    }
+
+    @Test
+    public void whenItemsAreFoundByID() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("test1");
+        Item second = new Item("test2");
+        Item third = new Item("test3");
+        Item fourth = new Item("test2");
+        Item fifth = new Item("test5");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(third);
+        tracker.add(fourth);
+        tracker.add(fifth);
+        String id = second.getId();
+        assertThat(tracker.findById(id), is(second));
+    }
+
+    @Test
+    public void whenItemsAreNotFoundByID() {
+        Tracker tracker = new Tracker();
+        Item first = new Item("test1");
+        Item second = new Item("test2");
+        Item third = new Item("test3");
+        Item fourth = new Item("test2");
+        Item fifth = new Item("test5");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(third);
+        tracker.add(fourth);
+        tracker.add(fifth);
+        assertNull(tracker.findById("ggggg"));
     }
 }
