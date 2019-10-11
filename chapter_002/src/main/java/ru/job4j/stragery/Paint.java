7c380a48fc4d7d767f94a13b0c9e7021f419f875
Anna <anabrikn@gmail.com>
2019-10-11 23:53:57 +0300

    [#786]

--- chapter_002/src/main/java/ru/job4j/stragery/Paint.java
+++ chapter_002/src/main/java/ru/job4j/stragery/Paint.java
@@ -1,6 +1,12 @@
 package ru.job4j.stragery;
 
 public class Paint {
+    public static void main(String[] args) {
+        Paint paint = new Paint();
+        paint.draw(new Triangle());
+        paint.draw(new Square());
+    }
+
     public void draw(Shape shape) {
         System.out.println(shape.draw());
     }
