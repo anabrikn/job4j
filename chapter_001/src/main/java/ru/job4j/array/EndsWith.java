1e1fd0e9b5b3337fdcc07343df44b76a10e4ddfe
Anna <anabrikn@gmail.com>
2019-10-28 00:45:51 +0300

    [#156317]

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = false;
+        int index = word.length - post.length;
+        for (int i = post.length - 1; i >= 0; i--) {
+            result = true;
+            if(word[index + i] != post[i]) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
