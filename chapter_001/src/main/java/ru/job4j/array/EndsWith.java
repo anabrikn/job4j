7acbe05d9346b323564c36c026b6d449e7039aee
Anna <anabrikn@gmail.com>
2019-10-28 01:37:14 +0300

    [#34117]

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -6,7 +6,7 @@ public class EndsWith {
         int index = word.length - post.length;
         for (int i = post.length - 1; i >= 0; i--) {
             result = true;
-            if(word[index + i] != post[i]) {
+            if (word[index + i] != post[i]) {
                 result = false;
                 break;
             }
--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -21,4 +21,17 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        if (finish <= data.length && start < finish) {
+            for (int i = start; i <= finish; i++) {
+                if (data[i] == el) {
+                    rst = i;
+                    break;
+                }
+            }
+        }
+        return rst;
+    }
 }
