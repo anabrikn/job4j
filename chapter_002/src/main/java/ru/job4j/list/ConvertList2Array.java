f47f0248311d1c413b960e62ff1f086eb5143588
Anna <anabrikn@gmail.com>
2019-11-10 17:04:57 +0300

    [#10035]

--- chapter_002/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_002/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -6,13 +6,15 @@ public class ConvertList2Array {
     public int[][] toArray(List<Integer> list, int rows) {
         int cells = (int) Math.ceil((double) list.size() / rows);
         int[][] array = new int[rows][cells];
-        int index = 0;
-        for (int[] elements: array) {
-            for (int j = 0; j < cells; j++) {
-                elements[j] = index < list.size() ? list.get(index) : 0;
-                index++;
+        int i = 0;
+        int j = 0;
+        for (Integer elements: list) {
+            array[j][i] = elements;
+            i++;
+            if (i % cells == 0) {
+                i = 0;
+                j++;
             }
-
         }
         return array;
     }
