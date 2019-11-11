681c75dcc462cb84ccd81b337eedf09607479a02
Anna <anabrikn@gmail.com>
2019-11-11 18:15:26 +0300

    [#10039]

--- chapter_002/src/main/java/ru/job4j/newtracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/newtracker/Tracker.java
@@ -1,17 +1,32 @@
 package ru.job4j.newtracker;
+import java.util.ArrayList;
+import java.util.ListIterator;
 import java.util.Random;
 
 public class Tracker {
-    private final Item[] items = new Item[100];
+    private final ArrayList<Item> items = new ArrayList<>(100);
     private int position = 0;
 
     public Item add(Item item) {
         item.setId(this.generateId());
-        this.items[this.position++] = item;
+        items.add(position++, item);
         return item;
     }
 
     public boolean replace(String id, Item item) {
+        boolean isReplace = false;
+        int i = 0;
+        for (ListIterator<Item> iterator = items.listIterator(); iterator.hasNext(); i++) {
+            Item itemForReplace = iterator.next();
+            if (itemForReplace.getId().equals(id)) {
+                item.setId(itemForReplace.getId());
+                items.add(i, item);
+                isReplace = true;
+                break;
+            }
+        }
+        return isReplace;
+        /*
         boolean isReplace = false;
         for (int i = 0; i != this.position; i++) {
             if (items[i].getId().equals(id)) {
@@ -21,10 +36,21 @@ public class Tracker {
                 break;
             }
         }
-        return isReplace;
+         */
     }
 
     public boolean delete(String id) {
+        boolean result = false;
+        for (ListIterator<Item> iterator = items.listIterator(); iterator.hasNext();) {
+            Item itemForDelete = iterator.next();
+            if (itemForDelete.getId().equals(id)) {
+                result = true;
+                items.remove(itemForDelete);
+                break;
+            }
+        }
+        return result;
+        /*
         boolean result = false;
         for (int i = 0; i < items.length; i++) {
             if (items[i].getId().equals(id)) {
@@ -35,9 +61,12 @@ public class Tracker {
             }
         }
         return result;
+         */
     }
 
     public Item[] findAll() {
+        return items.toArray(new Item[items.size()]);
+        /*
         Item[] resultArr = new Item[position];
         for (int i = 0; i != this.position; i++) {
             if (this.items[i] != null) {
@@ -45,9 +74,19 @@ public class Tracker {
             }
         }
         return resultArr;
+         */
     }
 
     public Item[] findByName(String key) {
+        ArrayList<Item> arrayList = new ArrayList<>();
+        for (ListIterator<Item> iterator = items.listIterator(); iterator.hasNext();) {
+            Item itemForFind = iterator.next();
+            if (itemForFind.getName().equals(key)) {
+                arrayList.add(itemForFind);
+            }
+        }
+        return arrayList.toArray(new Item[arrayList.size()]);
+        /*
         Item[] arr = new Item[position];
         int k = 0;
         for (int i = 0; i != this.position; i++) {
@@ -59,6 +98,7 @@ public class Tracker {
         Item[] resultArray = new Item[k];
         System.arraycopy(arr, 0, resultArray, 0, resultArray.length);
         return resultArray;
+         */
     }
 
     public Item findById(String id) {
