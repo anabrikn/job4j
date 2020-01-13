d127183dbf8d66325b98ea9e4758a6e68a0880ed
Anna <anabrikn@gmail.com>
2020-01-13 03:11:02 +0300

    [#24260]

--- chapter_002/src/main/java/ru/job4j/bank/core/Bank.java
+++ chapter_002/src/main/java/ru/job4j/bank/core/Bank.java
@@ -4,10 +4,7 @@ import ru.job4j.bank.exceptions.ContainsException;
 import ru.job4j.bank.parts.Account;
 import ru.job4j.bank.parts.User;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Bank {
     private TreeMap<User, ArrayList<Account>> bank = new TreeMap<>();
@@ -41,7 +38,7 @@ public class Bank {
 
     public void addAccountToUser(String passport, Account account) {
         User user = findUserByPassport(passport);
-        if (user == null || account == null) {
+        if (user == null || account == null) { // .ifPresent заменяет user != null
             throw new ContainsException("Такого пользователя нет в базе или переданы некорректные данные аккаунта.");
         }
         bank.get(user).add(account);
@@ -74,8 +71,14 @@ public class Bank {
     }
 
     private Account findAccountByPassportAndRequisite(String pass, String requisite) {
+        return getUserAccounts(pass)
+                .stream()
+                .filter(account -> account.getRequisites().equals(requisite))
+                .findFirst()
+                .orElse(null);
+
+        /*
         User u = findUserByPassport(pass);
-        Account a = null;
         if (bank.containsKey(u)) {
             for (Account array : bank.get(u)) {
                 if (array.getRequisites().equals(requisite)) {
@@ -84,10 +87,17 @@ public class Bank {
                 }
             }
         }
-        return a;
+         */
     }
 
-    private User findUserByPassport(String pass) {
+    private User findUserByPassport(String pass) { //stream ?
+        return bank.keySet()
+                .stream()
+                .filter(user -> user.getPassport().equals(pass))
+                .findFirst() //https://vertex-academy.com/tutorials/ru/java-8-stream-find/
+                .orElse(null); //https://habr.com/ru/post/225641/
+
+        /*
         User u = null;
         for (Map.Entry<User, ArrayList<Account>> entry : bank.entrySet()) {
             if (entry.getKey().getPassport().equals(pass)) {
@@ -95,9 +105,10 @@ public class Bank {
             }
         }
         return u;
+         */
     }
 
-    private Account findAccountByRequisite(String req) throws ContainsException {
+    private Account findAccountByRequisite(String req) throws ContainsException { //stream ?
         Account a = null;
         for (Map.Entry<User, ArrayList<Account>> entry : bank.entrySet()) {
             for (Account account: entry.getValue()) {
@@ -114,3 +125,19 @@ public class Bank {
         }
     }
 }
+
+/*
+Stream API упрощает работу с наборами данных,
+в частности, операции фильтрации, сортировки и другие манипуляции с данными.
+    1.  начать с создания экземпляра Stream,
+который опирается на нужную нам коллекцию, массив или метод их и откуда соответственно будут браться данные
+    2. операторы (по сути методы класса Stream)
+
+Операторы можно разделить на две группы:
+    Промежуточные (“intermediate”, ещё называют “lazy”) — обрабатывают поступающие элементы и возвращают стрим.
+Промежуточных операторов в цепочке обработки элементов может быть много.
+filter(Predicate predicate), map(Function mapper), flatMap(Function<T, Stream<R>> mapper)
+    Терминальные (“terminal”, ещё называют “eager”) — обрабатывают элементы и завершают работу стрима,
+так что терминальный оператор в цепочке может быть только один.
+forEach(Consumer action), System.out.println(stream.count()), collect(Collector collector), reduce(BinaryOperator accumulator)
+ */
