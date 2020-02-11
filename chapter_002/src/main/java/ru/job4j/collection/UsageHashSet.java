7f27cab9af30c1f1d81a90ac0d5982d616ea2887
Anna <anabrikn@gmail.com>
2020-02-11 05:59:04 +0300

    [#10095]

---
+++ chapter_002/src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -0,0 +1,10 @@
+package ru.job4j.collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UsageHashSet {
+    public static void main(String[] args) {
+        HashSet<String> autos = new HashSet<>(Set.of("Lada", "BMW", "Volvo", "Toyota"));
+        autos.forEach(System.out::println);
+    }
+}
