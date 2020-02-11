fab668c52462f2575183968c9dccf50e487bc5db
Anna <anabrikn@gmail.com>
2020-02-11 06:01:51 +0300

    [#212653]

---
+++ chapter_002/src/main/java/ru/job4j/collection/FullSearch.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FullSearch {
+    public static HashSet<String> extractNumber(List<Task> list) {
+        return (HashSet<String>) list
+                .stream()
+                .map(Task::getNumber)
+                .collect(Collectors.toSet());
+    }
+}
