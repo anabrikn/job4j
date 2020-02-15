a6d53fcd5fd2b2ef3edac5ec4cfbb5aee3ade477
Anna <anabrikn@gmail.com>
2020-02-15 04:45:30 +0300

    [#41598]

--- chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -2,6 +2,8 @@ package ru.job4j.search;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class PhoneDictionary {
     private List<Person> persons = new ArrayList<Person>();
@@ -13,10 +15,21 @@ public class PhoneDictionary {
     /**
      * Вернуть список всех пользователей, который содержат key в любых полях.
      * @param key Ключ поиска.
-     * @return Список подощедщих пользователей.
+     * @return Список подошедщих пользователей.
      */
     public List<Person> find(String key) {
-        List<Person> result = new ArrayList<>();
+
+        List<Person> result = persons.stream()
+                .flatMap(Stream::ofNullable)
+                .filter(person -> person.getName().contains(key)
+                        || person.getSurname().contains(key)
+                        || person.getAddress().contains(key)
+                        || person.getPhone().contains(key))
+                .collect(Collectors.toList());
+        return result;
+    }
+}
+/*List<Person> resultQ = new ArrayList<>();
         for (int i = 0; i < persons.size(); i++) {
             Person person = persons.get(i);
             String[] data = {person.getName(), person.getSurname(), person.getPhone(), person.getAddress()};
@@ -27,6 +40,5 @@ public class PhoneDictionary {
                 }
             }
         }
-        return result;
-    }
-}
+
+         */
