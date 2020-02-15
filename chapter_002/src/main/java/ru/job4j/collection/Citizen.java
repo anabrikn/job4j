60eceffa6592dd687819ca893fb84e64257cd0d3
Anna <anabrikn@gmail.com>
2020-02-15 04:08:52 +0300

    [#212656]

---
+++ chapter_002/src/main/java/ru/job4j/collection/Citizen.java
@@ -0,0 +1,40 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class Citizen {
+    private String passport;
+    private String username;
+
+    public Citizen(String passport, String username) {
+        this.passport = passport;
+        this.username = username;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Citizen)) {
+            return false;
+        }
+        Citizen citizen = (Citizen) o;
+        return getPassport().equals(citizen.getPassport());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(passport);
+    }
+
+    //
+}
